ac0dc8afaa4da5d9962fcb3396cc05ca36755418
unknown <m.rojas@utp.edu.co>
2020-03-29 20:13:12 -0500

    Se modifica algunos campos para cliente, falta aun validacion con la base de datos

--- LaneGo - Master/trouceApi/Models/Clients.cs
+++ LaneGo - Master/trouceApi/Models/Clients.cs
@@ -7,10 +7,15 @@ namespace trouceApi.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Lastname { get; set; }
-        public string Phone { get; set; }
-        public string Address { get; set; }
+        public int Nit { get; set; }
+        public int? Clienttype { get; set; }
+        public string Companyname { get; set; }
+        public string Representativename { get; set; }
         public string Email { get; set; }
+        public string Contactnumber { get; set; }
+        public string City { get; set; }
+        public string Comercialactivity { get; set; }
+        
         public string Password { get; set; }
     }
 }
--- LaneGo - Master/trouceApi/Models/trouceContext.cs
+++ LaneGo - Master/trouceApi/Models/trouceContext.cs
@@ -23,6 +23,7 @@ namespace trouceApi.Models
         public virtual DbSet<Usergenres> Usergenres { get; set; }
         public virtual DbSet<Userinstruments> Userinstruments { get; set; }
         public virtual DbSet<Users> Users { get; set; }
+        public virtual DbSet<Clients> Clients { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
@@ -154,6 +155,55 @@ namespace trouceApi.Models
                 entity.Property(e => e.Instrument)
                     .HasColumnName("instrument")
                     .HasColumnType("int(11)");
+            });
+
+            modelBuilder.Entity<Clients>(entity =>
+            {
+                entity.Property(e => e.Id)
+                    .HasColumnName("id")
+                    .HasColumnType("int(11)");
+
+                entity.Property(e => e.Clienttype)
+                    .HasColumnName("clienttype")
+                    .HasColumnType("int(11)");
+
+                entity.Property(e => e.Nit)
+                    .HasColumnName("nit")
+                    .HasColumnType("int(15)");
+
+                entity.Property(e => e.Companyname)
+                    .HasColumnName("companyname")
+                    .HasColumnType("varchar(30)");
+
+                entity.Property(e => e.Representativename)
+                    .HasColumnName("representativename")
+                    .HasColumnType("varchar(20)");
+
+                entity.Property(e => e.Email)
+                    .HasColumnName("email")
+                    .HasColumnType("varchar(60)");
+
+                entity.Property(e => e.Contactnumber)
+                    .HasColumnName("contactnumber")
+                    .HasColumnType("varchar(20)");
+
+                entity.Property(e => e.City)
+                    .HasColumnName("city")
+                    .HasColumnType("varchar(30)");
+
+
+                entity.Property(e => e.Comercialactivity)
+                    .HasColumnName("comercialactivity")
+                    .HasColumnType("longtext");
+
+
+                entity.Property(e => e.Name)
+                    .HasColumnName("name")
+                    .HasColumnType("varchar(30)");
+
+                entity.Property(e => e.Password)
+                    .HasColumnName("password")
+                    .HasColumnType("text");
             });
 
             modelBuilder.Entity<Users>(entity =>
